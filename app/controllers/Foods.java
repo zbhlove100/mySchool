5775574d54f23865900289b2a6814bd2dfe85269
bohan zhang <zbhlove100@gmail.com>
2011-12-05 23:34:19 +0800

    fix the show page css style

--- app/controllers/Foods.java
+++ app/controllers/Foods.java
@@ -3,6 +3,7 @@ package controllers;
 import java.util.List;
 
 import models.Food;
+import models.Materials;
 
 public class Foods extends BasicCrud{
 		
@@ -13,6 +14,16 @@ public class Foods extends BasicCrud{
 		public static void show(Long id){
 			
 			Food food = Food.findById(id);
+			List<Materials> mainMaterials = Materials.find("from FoodMaterials A ,Materials B where A.food.id=? and A.material.id = B.id and A.foodMaterialsType=?",
+					id,"main").fetch();
+			List<Materials> auxMaterials =  Materials.find("from FoodMaterials A ,Materials B where A.food.id=? and A.material.id = B.id and A.foodMaterialsType=?",
+					id,"auxiliary").fetch();
+			List<Materials> seaMaterials =  Materials.find("from FoodMaterials A ,Materials B where A.food.id=? and A.material.id = B.id and A.foodMaterialsType=?",
+					id,"seasoning").fetch();
+			renderArgs.put("mainMaterials", mainMaterials);
+			renderArgs.put("auxMaterials", auxMaterials);
+			renderArgs.put("seaMaterials", seaMaterials);
+			
 			render(food);
 		}
 }
