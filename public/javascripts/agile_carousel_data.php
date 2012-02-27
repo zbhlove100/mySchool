c269e4158b86f2986a863d13a033a1644c7e2d48
zhangbohan <bohan.zhang@samsung.com>
2012-02-27 18:04:08 +0800

    modify the login area in the index.html and add carousel

--- public/javascripts/agile_carousel_data.php
+++ public/javascripts/agile_carousel_data.php
@@ -1,10 +1,7 @@
 [{
-      "content": "<div class='slide_inner'><a class='photo_link' href='/foods/show/1'><img class='photo' src='/public/images/35628.3.jpg' alt='Bike' width='600px' height='300px'></a><a class='caption' href='#'>Sample Carousel Pic Goes Here And The Best Part is that...</a></div>",
-      "content_button": "<div class='thumb'><img src='/public/images/f2_thumb.jpg' alt='bike is nice'></div><p>pircture1</p>"
+      "content": "<div class='slide_inner'><a class='photo_link' href='/foods/show/1'><img class='photo' src='/public/styles/images/ui-icons_2e83ff_256x240.png' alt='Bike' width='750px' height='180px'></a><a class='caption' href='#'>Sample Carousel Pic Goes Here And The Best Part is that...</a></div>"
 }, {
-      "content": "<div class='slide_inner'><a class='photo_link' href='/foods/show/3'><img class='photo'  src='/public/images/35781.1.jpg' alt='Paint' width='600px' height='300px'></a><a class='caption' href=''>Sample Carousel Pic Goes Here And The Best Part is that...</a></div>",
-      "content_button": "<div class='thumb'><img src='/public/images/f2_thumb.jpg' alt='bike is nice'></div><p>picture2</p>"
+      "content": "<div class='slide_inner'><a class='photo_link' href='/foods/show/3'><img class='photo'  src='/public/styles/img/glyphicons-halflings.png' alt='Paint' width='750px' height='180px'></a><a class='caption' href=''>Sample Carousel Pic Goes Here And The Best Part is that...</a></div>"
 }, {
-      "content": "<div class='slide_inner'><a class='photo_link' href='/foods/show/4'><img class='photo' src='/public/images/1ad64eb7fa17eb39b66927ee28ea0d18.jpg' alt='Paint'></a><a class='caption' href='#'>Sample Carousel Pic Goes Here And The Best Part is that...</a></div>",
-      "content_button": "<div class='thumb'><img src='/public/images/f2_thumb.jpg' alt='bike is nice'></div><p>picture3</p>"
+      "content": "<div class='slide_inner'><a class='photo_link' href='/foods/show/4'><img class='photo' src='/public/styles/img/glyphicons-halflings-white.png' alt='Paint' width='750px' height='180px'></a><a class='caption' href='#'>Sample Carousel Pic Goes Here And The Best Part is that...</a></div>"
 }]
