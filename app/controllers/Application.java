d54d998be9dbd1d8b79f27f39775e536cb77ac86
zhangbohan <bohan.zhang@samsung.com>
2012-02-29 18:06:53 +0800

    .

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -10,11 +10,16 @@ import models.*;
 public class Application extends BasicCrud {
 
     public static void index() {
+    	loadHead();
+        render();
+    }
+    
+    public static Map loadHead(){
     	List<Teacher> teachers = Teacher.find("state", "main").fetch();
     	System.err.println(teachers.size());
     	Teacher bigTeacher = Teacher.find("state", "big").first();
     	renderArgs.put("bigTeacher", bigTeacher);
     	renderArgs.put("teachers",teachers);
-        render();
+    	return new HashMap();
     }
 }
---
+++ app/controllers/Teachers.java
@@ -0,0 +1,9 @@
+package controllers;
+
+public class Teachers extends BasicCrud{
+
+	public static void teacherDetail(){
+		Application.loadHead();
+		render();
+	}
+}
